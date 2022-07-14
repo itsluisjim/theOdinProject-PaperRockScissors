fe18848d7029252ef1e5704091fafd5cffdf6b8e
Luis Jimenez <eternal_pitta0b@icloud.com>
2022-07-14 15:20:28 -0500

    Create function that plays one game round

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ function computerPlay(){
 
     return computerOption;
 }
+
+function playRound (playerSelection, computerSelection){
+
+    if(playerSelection.toLowerCase() == 'rock'){
+        switch(computerSelection){
+            case "paper":
+                return "You lost! Paper beats rock";
+            case "scissors":
+                return "You won! Rock beats scissors";
+            default:
+                return "Tie";
+        }
+    }
+    else if (playerSelection.toLowerCase() == 'paper'){
+        switch(computerSelection){
+            case "rock":
+                return "You won! Paper beats rock";
+            case "scissors":
+                return "You lost! Scissors beats paper";
+            default:
+                return "Tie";
+        }
+    }
+    else if (playerSelection.toLowerCase() == 'scissors'){
+        switch(computerSelection){
+            case "rock":
+                return "You lost! Rock beats scissors";
+            case "paper":
+                return "You won! Scissors beats paper";
+            default:
+                return "Tie";
+        }
+    }
+    else {
+        return "Not a valid answer!";
+    }
+}
