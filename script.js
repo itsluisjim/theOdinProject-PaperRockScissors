3c97aeedf6e445089c238fec46deb1dcb60bf17e
Luis Jimenez <eternal_pitta0b@icloud.com>
2022-07-14 17:55:26 -0500

    Prevent user from entering invalid output

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function computerPlay(){
 the computer as arguments and returns a string stating who won and why they won */
 function playRound (playerSelection, computerSelection){
 
-    if(playerSelection.toLowerCase() == 'rock'){
+    if(playerSelection == 'rock'){
         switch(computerSelection){
             case "paper":
                 return "You lost! Paper beats rock";
@@ -20,7 +20,7 @@ function playRound (playerSelection, computerSelection){
                 return "Tie";
         }
     }
-    else if (playerSelection.toLowerCase() == 'paper'){
+    else if (playerSelection == 'paper'){
         switch(computerSelection){
             case "rock":
                 return "You won! Paper beats rock";
@@ -30,7 +30,7 @@ function playRound (playerSelection, computerSelection){
                 return "Tie";
         }
     }
-    else if (playerSelection.toLowerCase() == 'scissors'){
+    else {
         switch(computerSelection){
             case "rock":
                 return "You lost! Rock beats scissors";
@@ -39,9 +39,6 @@ function playRound (playerSelection, computerSelection){
             default:
                 return "Tie";
         }
-    }
-    else {
-        return "Not a valid answer!";
     }
 }
 
@@ -59,28 +56,33 @@ function game() {
     while(gameRunner){
 
         // prompts user for a choice and stores in variable
-        let userSelection = prompt("paper, rock, or scissors? ");
+        let userSelection = prompt("paper, rock, or scissors? ").toLowerCase();
 
-        // plays one round, and returns a string stating who won and stores in a variable
-        let matchResult = playRound(userSelection, computerPlay());
+        if(userSelection == 'paper'|| userSelection == 'rock' || userSelection == 'scissors'){
+            // plays one round, and returns a string stating who won and stores in a variable
+            let matchResult = playRound(userSelection, computerPlay());
 
-        //seaches if the string returned by playRound has 'You won' or 'You lost' and adds a point accordingly
-        if(matchResult.search('You won') >= 0){
-            userPoints+=1;
-            console.log("You win a point!");
-        }
-        else if(matchResult.search('You lost') >= 0){
-            computerPoints+=1;
-            console.log("Computer wins a point!");
+            //seaches if the string returned by playRound has 'You won' or 'You lost' and adds a point accordingly
+            if(matchResult.search('You won') >= 0){
+                userPoints+=1;
+                console.log("You win a point!");
+            }
+            else if(matchResult.search('You lost') >= 0){
+                computerPoints+=1;
+                console.log("Computer wins a point!");
+            }
+            else {
+                console.log("Tie no points for either party");
+            }
+
+            /* checks if any player has reached 5 points, if so it will
+            change the condition that runs the while loop and end it */
+            if(userPoints == 5 || computerPoints == 5){
+                gameRunner = false;
+            }
         }
         else {
-            console.log("Tie no points for either party");
-        }
-
-        /* checks if any player has reached 5 points, if so it will
-        change the condition that runs the while loop and end it */
-        if(userPoints == 5 || computerPoints == 5){
-            gameRunner = false;
+            window.alert("Invalid choice! Try agian!")
         }
     }
 
