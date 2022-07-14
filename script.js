03d53d9578d6d58f4ea79d14408caf9590a55783
Luis Jimenez <eternal_pitta0b@icloud.com>
2022-07-14 15:24:17 -0500

    Create game() that runs till 5 points are scored

--- script.js
+++ script.js
@@ -42,3 +42,39 @@ function playRound (playerSelection, computerSelection){
         return "Not a valid answer!";
     }
 }
+
+function game() {
+
+    let userPoints = 0;
+    let computerPoints= 0;
+    let gameRunner = true;
+
+    while(gameRunner){
+        let userSelection = prompt("paper, rock, or scissors? ");
+
+        let matchResult = playRound(userSelection, computerPlay());
+
+        if(matchResult.search('You won') >= 0){
+            userPoints+=1;
+            console.log("You win a point!");
+        }
+        else if(matchResult.search('You lost') >= 0){
+            computerPoints+=1;
+            console.log("Computer wins a point!");
+        }
+        else {
+            console.log("Tie no points for either party");
+        }
+
+        if(userPoints == 5 || computerPoints == 5){
+            gameRunner = false;
+        }
+    }
+
+    if(userPoints > computerPoints){
+        console.log("You won with " + userPoints + "points");
+    }
+    else {
+        console.log("Computer won with " + computerPoints + "points");
+    }
+}
