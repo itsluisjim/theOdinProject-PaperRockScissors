6b5d7b29682c705a9a53afd98b4447ca675bf3e3
Luis Jimenez <eternal_pitta0b@icloud.com>
2022-07-14 16:03:31 -0500

    Add comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// computerPlay() randonly picks a choice for the computer
 function computerPlay(){
     const gameChoices = ['paper','rock','scissors'];
 
@@ -5,7 +6,8 @@ function computerPlay(){
 
     return computerOption;
 }
-
+/* playRound() takes user choice and the randomly generated choice for
+the computer as arguments and returns a string stating who won and why they won */
 function playRound (playerSelection, computerSelection){
 
     if(playerSelection.toLowerCase() == 'rock'){
@@ -43,17 +45,26 @@ function playRound (playerSelection, computerSelection){
     }
 }
 
+// calls for a game of paper, rock, scissors
 function game() {
 
+    // keeps track of user and computers points
     let userPoints = 0;
     let computerPoints= 0;
+
+    //controls the while loop
     let gameRunner = true;
 
+    // while loop will run until a player reaches 5 points
     while(gameRunner){
+
+        // prompts user for a choice and stores in variable
         let userSelection = prompt("paper, rock, or scissors? ");
 
+        // plays one round, and returns a string stating who won and stores in a variable
         let matchResult = playRound(userSelection, computerPlay());
 
+        //seaches if the string has 'You won' or 'You lost' and adds a point accordingly
         if(matchResult.search('You won') >= 0){
             userPoints+=1;
             console.log("You win a point!");
@@ -66,11 +77,14 @@ function game() {
             console.log("Tie no points for either party");
         }
 
+        /* checks if any player has reached 5 points, if so it will
+        change the condition that runs the while loop and end it */
         if(userPoints == 5 || computerPoints == 5){
             gameRunner = false;
         }
     }
 
+    // checks who has more points and prints a message staing who won
     if(userPoints > computerPoints){
         console.log("You won with " + userPoints + "points");
     }
